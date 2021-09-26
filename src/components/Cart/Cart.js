af862404d396d2f1d86a55235953fbdf297eeadb
DEWAN <dewanmahinchy@gmail.com>
2021-09-26 12:48:29 +0600

    all file added

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Cart.css'
+
+const Cart = (props) => {
+    const {cart} = props;
+    let total = 0;
+    for(const product of cart){
+        total += product.price;
+    }
+
+    const shipping = 15;
+    const tax = (total + shipping) * 0.1;
+    const grandTotal = total + shipping + tax;
+
+    return (
+        <div>
+            <h3>Order Summary</h3>
+            <h5>Items Ordered: {props.cart.length}</h5>
+            <br />
+            <p>Total: {total.toFixed(2)}</p>
+            <p>Shipping: {shipping}</p>
+            <p>Tax: {tax.toFixed(2)}</p>
+            <p>Grand Total: {grandTotal.toFixed(2)}</p>
+        </div>
+    );
+};
+
+export default Cart;
